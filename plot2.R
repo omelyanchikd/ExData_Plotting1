54f3d42b10eee4d271a00e99b29715f26183575e
omelyanchikd <omelyanchikd@ukr.net>
2016-04-15 21:50:48 +0200

    done

---
+++ plot2.R
@@ -0,0 +1,42 @@
+# ask for name of the file with the source data
+filename <- readline("Please specify a name of the file you want to upload (including path): ")
+
+# default values for runnning on my computer
+if (filename == "")
+  filename <- "C:/Users/Diana/Downloads/household_power_consumption.txt"
+
+# read data (only the lines, which correspond to the dates of interest)
+input <- read.table(filename, header = TRUE, sep = ";", dec = ".", na.strings = "?", 
+                    stringsAsFactors = FALSE, skip = 66636, nrows = 2880)
+
+# change column names to appropriate ones
+names(input) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", 
+                  "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+
+# date should be recognised as a date
+input$Date <- as.Date(input$Date, format = "%d/%m/%Y")
+
+# redundant check (that's how I know which lines to skip and how many rows to read)
+input<- input[input$Date >= as.Date("2007-02-01") & input$Date <= as.Date("2007-02-02"),]
+
+# we need new variable to be able to plot points like Thursday, Feb 1, 00:00
+input$datetime <- strptime(paste(input$Date, input$Time), format = "%Y-%m-%d %H:%M:%S")
+
+# needed if you are not using English locale
+Sys.setlocale("LC_TIME","English")
+
+# select destination for new chart
+output<- readline("Specify a folder to save created chart: ")
+if (output == "")
+  output<-"D:/R Programming/ExData_Plotting1/"
+
+# specify graphical device to save chart
+png(filename = paste(output, "plot2.png"))
+
+#create chart
+plot(input$datetime, input$Global_active_power, type = "l", xlab = "",
+     ylab = "Global Active Power (kilowatts)")
+
+#turn off device after usage to save chart
+dev.off()
+
